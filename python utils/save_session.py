9529ea12c3b8a09db5bdffb53ba50580da92bf44
f1quant <ruilov@gmail.com>
2026-03-09 07:32:34 -0400

    aus 26

--- python utils/save_session.py
+++ python utils/save_session.py
@@ -158,48 +158,48 @@ my_f1_utils.setup_cache(offline=False)
 
 # =============== SAVING RACES OR SPRINTS ===============
 # all races to reconstruct everything
-# races = []
-# years = list(range(2025,2017,-1))
-# rounds = list(range(26,0,-1))
-# session_types = ["R","S"]
-# for year in years:
-#     for round_no in rounds:
-#         for session_type in session_types:
-#             races.append((year, round_no, session_type))
-
-# races = [(2025,24,"R")]
-# all_df_fn = "github/all_df.csv"
-# do_driver_info = True
-# do_all_df = True
-# do_timing_data = True
-
-# if do_driver_info:
-#     save_driver_info(races)
-
-# if do_all_df:
-#     save_to_all_df(races, all_df_fn)
-
-# if do_timing_data:
-#     save_timing_data(races)
-
-# =============== SAVING QUALIS ===============
-# all races to reconstruct everything
 races = []
 years = list(range(2025,2017,-1))
 rounds = list(range(26,0,-1))
-session_types = ["Q"]
+session_types = ["R","S"]
 for year in years:
     for round_no in rounds:
         for session_type in session_types:
             races.append((year, round_no, session_type))
 
-# races = [(2021,16,"Q")]
-all_df_fn = "github/all_df_q.csv"
+races = [(2026,1,"R")]
+all_df_fn = "github/all_df.csv"
 do_driver_info = True
 do_all_df = True
+do_timing_data = True
 
 if do_driver_info:
     save_driver_info(races)
 
 if do_all_df:
-    save_to_all_df(races, all_df_fn, quali=True)
+    save_to_all_df(races, all_df_fn)
+
+if do_timing_data:
+    save_timing_data(races)
+
+# =============== SAVING QUALIS ===============
+# all races to reconstruct everything
+# races = []
+# years = list(range(2025,2017,-1))
+# rounds = list(range(26,0,-1))
+# session_types = ["Q"]
+# for year in years:
+#     for round_no in rounds:
+#         for session_type in session_types:
+#             races.append((year, round_no, session_type))
+
+# # races = [(2021,16,"Q")]
+# all_df_fn = "github/all_df_q.csv"
+# do_driver_info = True
+# do_all_df = True
+
+# if do_driver_info:
+#     save_driver_info(races)
+
+# if do_all_df:
+#     save_to_all_df(races, all_df_fn, quali=True)
